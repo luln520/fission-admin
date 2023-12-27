477b56eae63129c933b1a8af22b8267aee2fbced
luln520 <1119076443luln@gmail.com>
2023-12-27 19:03:42 +0800

    dev上传

--- smart-admin-api/sa-service/src/main/java/service/TwCoinService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import entity.TwCoin;
-import entity.TwConfig;
-
-public interface TwCoinService extends IService<TwCoin> {
-}
