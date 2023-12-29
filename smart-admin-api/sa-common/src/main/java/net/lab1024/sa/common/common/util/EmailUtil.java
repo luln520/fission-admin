55a20d284f1355bd86e789c8cf2dbc22b4b7f66d
BaiSeBase <19102664454@163.com>
2023-12-29 23:00:26 +0800

    登陆注册

---
+++ smart-admin-api/sa-common/src/main/java/net/lab1024/sa/common/common/util/EmailUtil.java
@@ -0,0 +1,12 @@
+package net.lab1024.sa.common.common.util;
+
+/**
+ * 邮箱发送工具
+ */
+public class EmailUtil {
+
+    /**
+     *
+     */
+
+}
---
+++ smart-admin-api/sa-common/src/main/java/net/lab1024/sa/common/common/util/SMSUtil.java
@@ -0,0 +1,25 @@
+package net.lab1024.sa.common.common.util;
+
+import cn.hutool.http.HttpUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 短信发送工具
+ */
+public class SMSUtil {
+
+    /**
+     *
+     */
+
+}
