701449ba857cc1e4d88418db0901918acece4a68
BaiSeBase <19102664454@163.com>
2023-12-29 21:41:42 +0800

    先做 pc

---
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/LoginController.java
@@ -0,0 +1,17 @@
+package net.lab1024.sa.admin.module.system.TwPC.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户登陆
+ */
+@RestController
+@RequestMapping("/api/pc/userlogin")
+public class LoginController {
+
+    /**
+     * 用户登陆
+     * */
+}
+
