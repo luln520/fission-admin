1e93be511adf336af118f223a42e3e472e872045
BaiSeBase <19102664454@163.com>
2023-12-30 16:39:15 +0800

    通知  接口完善提取

--- smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/NoticeController.java
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/NoticeController.java
@@ -25,5 +25,34 @@ public class NoticeController {
      * select notice  where  id=?
      * */
 
+    /**
+     * 标记全部已经读取
+     * 参数：token
+     * 表 notice
+     * update notice set status=1  where  uid=?（通过 token 拿到id）
+     * */
+
+    /**
+     * 标记单个已经读取
+     * 参数：id(notice的id)
+     * 表 notice
+     * update notice set status=1  where  id=?
+     * */
+
+    /**
+     * 删除我的全部通知
+     * 参数： token
+     * 表 notice
+     * delete notice  where  uid=?（通过 token 拿到id）
+     * */
+
+    /**
+     * 删除我单个通知
+     * 参数： id(notice的id)
+     * 表 notice
+     * delete notice  where  id=?
+     * */
+
+
 }
 
