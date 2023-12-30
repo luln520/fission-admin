401275148874c94950e692b80da57b67a7dc2d8b
luln520 <1119076443luln@gmail.com>
2023-12-30 16:54:33 +0800

    重置密码挪位置

--- smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/UserController.java
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/UserController.java
@@ -34,11 +34,31 @@ public class UserController {
     }
 
     /**
-     * 通知详情
-     * 参数： id （ 公告 id）
-     * 表 notice
-     * select notice  where  id=?
+     * 用户重置密码  表 user，notice
+     * 参数： username  password  oldPassword（老密码）
+     * 大致逻辑：
+     *      1.通过用户名查询用户，先判断该用户是否存在
+     *      3.判断账号老密码是否正确MD5
+     *      4.都通过  MD5 加密password 修改password 字段
+     *      5.新增通知记录 （表 notice）
+     *
+     *
+     * 参考代码：
+     *      //通知写入
+     *       $data['uid'] = $uinfo['id'];（用户id）
+     *       $data['account'] = $uinfo['username']; （用户 username）
+     *       $data['title'] = '重置密码'
+     *       $data['content'] = '登陆密码重置成功';
+     *       $data['addtime'] = date("Y-m-d H:i:s", time());
+     *       $data['status'] = 1;
+     *       M("notice")->add($data); （写入通知表）
      * */
+    @NoNeedLogin
+    @PostMapping("/editpassword")
+    @ApiOperation("用户重置密码")
+    public ResponseDTO editpassword(@Valid @RequestBody UserReq userReq) {
+        return twUserService.editpassword(userReq);
+    }
 
 }
 
