a8b677a6ea4f75a02c0ce5dd503f717da73f975d
BaiSeBase <19102664454@163.com>
2023-12-30 15:49:12 +0800

    通知  和 用户部分 接口提取

---
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/NoticeController.java
@@ -0,0 +1,29 @@
+package net.lab1024.sa.admin.module.system.TwPC.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 通知
+ */
+@RestController
+@RequestMapping("/api/pc/notice")
+public class NoticeController {
+
+
+    /**
+     * 通知列表
+     * 表 notice
+     * 参数： id （用户 id）
+     * select notice  where  uid=？
+     * */
+
+    /**
+     * 通知详情
+     * 参数： id （ 公告 id）
+     * 表 notice
+     * select notice  where  id=?
+     * */
+
+}
+
---
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/UserController.java
@@ -0,0 +1,29 @@
+package net.lab1024.sa.admin.module.system.TwPC.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户
+ */
+@RestController
+@RequestMapping("/api/pc/user")
+public class UserController {
+
+
+    /**
+     * 获取用户信息
+     * 表 user
+     * 参数： token
+     * select user  where  uid=？
+     * */
+
+    /**
+     * 通知详情
+     * 参数： id （ 公告 id）
+     * 表 notice
+     * select notice  where  id=?
+     * */
+
+}
+
