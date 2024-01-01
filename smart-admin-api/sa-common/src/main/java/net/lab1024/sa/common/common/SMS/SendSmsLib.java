64e0de1449a600e0f1a7cbae3287adf6ca478853
luln520 <1119076443luln@gmail.com>
2024-01-01 17:08:55 +0800

    JAVA11

--- smart-admin-api/sa-common/src/main/java/net/lab1024/sa/common/common/SMS/SendSmsLib.java
+++ smart-admin-api/sa-common/src/main/java/net/lab1024/sa/common/common/SMS/SendSmsLib.java
@@ -1,13 +1,5 @@
 package net.lab1024.sa.common.common.SMS;
 
-import com.infobip.ApiClient;
-import com.infobip.ApiException;
-import com.infobip.ApiKey;
-import com.infobip.BaseUrl;
-import com.infobip.api.SmsApi;
-import com.infobip.model.SmsAdvancedTextualRequest;
-import com.infobip.model.SmsDestination;
-import com.infobip.model.SmsTextualMessage;
 import lombok.var;
 
 import java.util.Collections;
@@ -19,32 +11,32 @@ public class SendSmsLib {
     private static final String RECIPIENT = "639627665869";
 
     public static void main(String[] args) {
-        // Create the API client and the Send SMS API instances.
-        var apiClient = ApiClient.forApiKey(ApiKey.from(API_KEY))
-                .withBaseUrl(BaseUrl.from(BASE_URL))
-                .build();
-        var sendSmsApi = new SmsApi(apiClient);
-
-        // Create a message to send.
-        var smsMessage = new SmsTextualMessage()
-                .addDestinationsItem(new SmsDestination().to(RECIPIENT))
-                .text("Hello from Infobip Java Client!");
-
-        // Create a send message request.
-        var smsMessageRequest = new SmsAdvancedTextualRequest()
-                .messages(Collections.singletonList(smsMessage));
-
-        try {
-            // Send the message.
-            var smsResponse = sendSmsApi.sendSmsMessage(smsMessageRequest).execute();
-            System.out.println("Response body: " + smsResponse);
-
-            // Get delivery reports. It may take a few seconds to show the above-sent message.
-            var reportsResponse = sendSmsApi.getOutboundSmsMessageDeliveryReports().execute();
-            System.out.println(reportsResponse.getResults());
-        } catch (ApiException e) {
-            System.out.println("HTTP status code: " + e.responseStatusCode());
-            System.out.println("Response body: " + e.rawResponseBody());
-        }
+//        // Create the API client and the Send SMS API instances.
+//        var apiClient = ApiClient.forApiKey(ApiKey.from(API_KEY))
+//                .withBaseUrl(BaseUrl.from(BASE_URL))
+//                .build();
+//        var sendSmsApi = new SmsApi(apiClient);
+//
+//        // Create a message to send.
+//        var smsMessage = new SmsTextualMessage()
+//                .addDestinationsItem(new SmsDestination().to(RECIPIENT))
+//                .text("Hello from Infobip Java Client!");
+//
+//        // Create a send message request.
+//        var smsMessageRequest = new SmsAdvancedTextualRequest()
+//                .messages(Collections.singletonList(smsMessage));
+//
+//        try {
+//            // Send the message.
+//            var smsResponse = sendSmsApi.sendSmsMessage(smsMessageRequest).execute();
+//            System.out.println("Response body: " + smsResponse);
+//
+//            // Get delivery reports. It may take a few seconds to show the above-sent message.
+//            var reportsResponse = sendSmsApi.getOutboundSmsMessageDeliveryReports().execute();
+//            System.out.println(reportsResponse.getResults());
+//        } catch (ApiException e) {
+//            System.out.println("HTTP status code: " + e.responseStatusCode());
+//            System.out.println("Response body: " + e.rawResponseBody());
+//        }
     }
 }
