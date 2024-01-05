1d6cede5d7648d3fc08f176264d2b56e9cf41203
luln520 <1119076443luln@gmail.com>
2024-01-05 20:34:39 +0800

    文件上传

--- smart-admin-api/sa-common/src/main/java/net/lab1024/sa/common/module/support/file/service/FileService.java
+++ smart-admin-api/sa-common/src/main/java/net/lab1024/sa/common/module/support/file/service/FileService.java
@@ -307,8 +307,8 @@ public class FileService {
 
     public String upload(MultipartFile file, String path, String fileName) throws Exception {
         // 生成新的文件名
-//        String realPath = path + "/" + UUID.randomUUID().toString().replace("-", "")+fileName.substring(fileName.lastIndexOf("."));
-        String realPath = "C:\\Users\\1\\Desktop\\fsdownload" + "/" + UUID.randomUUID().toString().replace("-", "")+fileName.substring(fileName.lastIndexOf("."));
+        String realPath = path + "/" + UUID.randomUUID().toString().replace("-", "")+fileName.substring(fileName.lastIndexOf("."));
+//        String realPath = "C:\\Users\\1\\Desktop\\fsdownload" + "/" + UUID.randomUUID().toString().replace("-", "")+fileName.substring(fileName.lastIndexOf("."));
         File dest = new File(realPath);
               // 判断文件父目录是否存在
         if (!dest.getParentFile().exists()) {
