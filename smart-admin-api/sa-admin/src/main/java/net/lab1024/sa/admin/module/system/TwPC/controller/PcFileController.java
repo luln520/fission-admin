7ce5625dc45d2471eaa3272c14254395f0fe6ef4
luln520 <1119076443luln@gmail.com>
2024-01-24 19:54:05 +0800

    登录权限 放开

--- smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/PcFileController.java
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/PcFileController.java
@@ -29,7 +29,7 @@ import javax.validation.Valid;
  */
 @RestController
 @Api(tags = {AdminSwaggerTagConst.PC.PC_FILE})
-@RequestMapping("/api/pc/")
+@RequestMapping("/api/pc/file")
 public class PcFileController extends SupportBaseController {
 
     @Autowired
