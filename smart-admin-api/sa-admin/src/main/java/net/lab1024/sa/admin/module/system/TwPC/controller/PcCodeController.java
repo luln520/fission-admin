49c15debefd6283659fa319df9f45433e11f72e2
luln520 <1119076443luln@gmail.com>
2024-01-24 15:27:23 +0800

    正式配置

--- smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/PcCodeController.java
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwPC/controller/PcCodeController.java
@@ -1,13 +1,8 @@
 package net.lab1024.sa.admin.module.system.TwPC.controller;
 
 
-import com.infobip.*;
-import com.infobip.api.SmsApi;
-import com.infobip.model.SmsDeliveryResult;
-import com.infobip.model.SmsReport;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.var;
 import net.lab1024.sa.admin.constant.AdminSwaggerTagConst;
 import net.lab1024.sa.admin.module.system.TwAdmin.service.TwUserService;
 import net.lab1024.sa.common.common.annoation.NoNeedLogin;
@@ -15,9 +10,7 @@ import net.lab1024.sa.common.common.domain.ResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/pc/code")
