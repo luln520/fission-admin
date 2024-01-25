334bd73eb539f168ba62ce1f5eee019ba3e2bd17
luln520 <1119076443luln@gmail.com>
2024-01-25 18:42:10 +0800

    矿机次数 BUG

--- smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwAdmin/service/impl/TwKuangjiServiceImpl.java
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/TwAdmin/service/impl/TwKuangjiServiceImpl.java
@@ -231,7 +231,7 @@ public class TwKuangjiServiceImpl extends ServiceImpl<TwKuangjiDao, TwKuangji> i
         queryKjorder.eq("kid",kuangji.getId());
         queryKjorder.eq("status",1);
         int count = twKjorderDao.selectCount(queryKjorder).intValue();
-        if(one.getNum().compareTo(count) < 0){
+        if(count <= one.getNum()  ){
             if(language.equals("zh")){
                 return ResponseDTO.userErrorParam("超出购买限制次数！");
             }else{
