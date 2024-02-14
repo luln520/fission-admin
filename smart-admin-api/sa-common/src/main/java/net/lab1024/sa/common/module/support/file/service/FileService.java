a2f4b3d2a2e7d7fccc1b5a8c89be1cb099c1e6f8
luln520 <1119076443luln@gmail.com>
2024-02-14 17:28:44 +0800

    提币审核

--- smart-admin-api/sa-common/src/main/java/net/lab1024/sa/common/module/support/file/service/FileService.java
+++ smart-admin-api/sa-common/src/main/java/net/lab1024/sa/common/module/support/file/service/FileService.java
@@ -283,7 +283,7 @@ public class FileService {
 
 
     public ResponseDTO editMovieInfo(MultipartFile file){
-        String uploadDir = "/opt/coin-admin/smart-admin-api/upload";
+        String uploadDir = "/opt/smart-admin-api/upload";
         try {
            // 图片路径
             String imgUrl = null;
