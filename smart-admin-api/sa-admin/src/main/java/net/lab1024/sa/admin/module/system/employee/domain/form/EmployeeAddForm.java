4bcf39c9188d41acd573c57355d0eeef9c1c80d6
luln520 <1119076443luln@gmail.com>
2024-04-21 01:04:18 +0800

    在线客服

--- smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/employee/domain/form/EmployeeAddForm.java
+++ smart-admin-api/sa-admin/src/main/java/net/lab1024/sa/admin/module/system/employee/domain/form/EmployeeAddForm.java
@@ -52,7 +52,6 @@ public class EmployeeAddForm {
 
     @ApiModelProperty("手机号")
     @NotNull(message = "手机号不能为空")
-    @Pattern(regexp = SmartVerificationUtil.PHONE_REGEXP, message = "手机号格式不正确")
     private String phone;
 
     @ApiModelProperty("角色列表")
